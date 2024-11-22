e34a516d37bb9cbbc2c26d752dc927036aca17cd
mahbub-alom <mdmahbuba034@gmail.com>
2024-11-22 18:43:46 +0600

    eslintrc.json file edit and details page props solution write done

--- src/app/(route)/details/[recordId]/page.tsx
+++ src/app/(route)/details/[recordId]/page.tsx
@@ -4,7 +4,15 @@ import React from "react";
 import DoctorDetails from "../_components/DoctorDetails";
 import DoctorSuggestionList from "../_components/DoctorSuggestionList";
 
-const Details = async ({ params }: { params: { recordId: string } }) => {
+// Define proper types for the params
+type DetailsProps = {
+  params: {
+    recordId: string;
+  };
+  searchParams?: { [key: string]: string | string[] | undefined };
+};
+
+const Details = async ({ params }: DetailsProps) => {
   const recordId = params?.recordId;
 
   const doctor = await GlobalApi.getDoctorById(recordId)
@@ -13,23 +21,6 @@ const Details = async ({ params }: { params: { recordId: string } }) => {
       console.log("error fetching doctor data", error);
       return null;
     });
-  // const { recordId } = use(params);
-  // const [doctor, setDoctor] = useState([]);
-  // useEffect(() => {
-  //   if (recordId) {
-  //     getDoctorById();
-  //   }
-  // }, [recordId]);
-
-  // const getDoctorById = () => {
-  //   GlobalApi.getDoctorById(recordId)
-  //     .then((resp) => {
-  //       setDoctor(resp.data);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error fetching doctor data:", error);
-  //     });
-  // };
 
   return (
     <div className="p-5 md:px-20">
